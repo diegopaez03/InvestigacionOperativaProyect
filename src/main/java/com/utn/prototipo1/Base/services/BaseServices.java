d369632ac1a41aed58580a09517f6fb471e788cb
luca-lagos <lagos.herrera.luca@gmail.com>
2024-05-29 19:31:04 -0300

    Casi listop

--- src/main/java/com/utn/prototipo1/Base/services/BaseServices.java
+++ src/main/java/com/utn/prototipo1/Base/services/BaseServices.java
@@ -5,7 +5,7 @@ import com.utn.prototipo1.Base.entities.BaseEntidad;
 import java.io.Serializable;
 import java.util.List;
 
-public interface BaseServices <E extends BaseEntidad, ID extends Serializable> {
+public interface BaseServices <E extends BaseEntidad, ID > {
     public List<E> findAll() throws Exception;
     public E findById(ID id) throws Exception;
     public E save(E entity) throws Exception;
