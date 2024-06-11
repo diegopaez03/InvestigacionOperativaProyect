305d3b60ef01534d534c5eb8fb000175a4a8164e
Benjamin Lagos <benjalagospato@gmail.com>
2024-06-11 00:08:21 -0300

    cambios benja

--- src/main/java/com/utn/prototipo1/moduloInventario/services/InventarioArticuloServiceImpl.java
+++ src/main/java/com/utn/prototipo1/moduloInventario/services/InventarioArticuloServiceImpl.java
@@ -2,8 +2,13 @@ package com.utn.prototipo1.moduloInventario.services;
 
 import com.utn.prototipo1.Base.repositories.BaseRepository;
 import com.utn.prototipo1.Base.services.BaseServicesImpl;
+import com.utn.prototipo1.moduloArticulo.entities.Articulo;
+import com.utn.prototipo1.moduloArticulo.repositories.ArticuloRepository;
 import com.utn.prototipo1.moduloInventario.entities.InventarioArticulo;
 import com.utn.prototipo1.moduloInventario.repositories.InventarioArticuloRepository;
+import com.utn.prototipo1.moduloOrdenCompra.repositories.OrdenDeCompraRepository;
+import com.utn.prototipo1.moduloVenta.entities.DetalleFactura;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -13,7 +18,30 @@ public class InventarioArticuloServiceImpl extends BaseServicesImpl<InventarioAr
     @Autowired
     private InventarioArticuloRepository inventarioArticuloRepository;
 
+    @Autowired
+    private ArticuloRepository articuloRepository;
+
+    @Autowired
+    private OrdenDeCompraRepository ordenDeCompraRepository;
+
     public InventarioArticuloServiceImpl(BaseRepository<InventarioArticulo, Long> baseRepository) {
         super(baseRepository);
+    }
+
+
+    @Override
+    public InventarioArticulo generarInventarioArticulo(long idArticulo) {
+        Articulo articulo = articuloRepository.findById(idArticulo).orElseThrow(() -> new EntityNotFoundException("Articulo not found"));
+
+        float cantidad = articulo.;
+        float total = cantidad*precio;
+
+        InventarioArticulo inventarioArticulo = new InventarioArticulo();
+        inventarioArticulo.setArticulo(articulo);
+        inventarioArticulo.setPuntoPedido();//verificar si su modelo es TipoLote
+        inventarioArticulo.setStockSeguridad();//y aca verificar cual modelo de los es.
+        inventarioArticulo.setLoteOptimo();//aca ver si es de TipoLote
+
+        return inventarioArticulo;
     }
 }
