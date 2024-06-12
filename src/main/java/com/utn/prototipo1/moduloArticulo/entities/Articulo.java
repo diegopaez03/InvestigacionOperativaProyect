8ab88e6fde91802e2f2a60990fc6b226b0bfe6f2
diegopaez03 <Diegosan016@gmail.com>
2024-06-12 12:30:33 -0300

    Revert "Probando inventario"
    This reverts commit 90af71bbc225e8b7290e4495a2b987cb84cedbe4.

--- src/main/java/com/utn/prototipo1/moduloArticulo/entities/Articulo.java
+++ src/main/java/com/utn/prototipo1/moduloArticulo/entities/Articulo.java
@@ -33,8 +33,8 @@ public class Articulo extends BaseEntidad {
     private Date fechaBaja;
 
 
-    @NonNull
-    @ManyToOne(optional = false)
+    @NotNull
+    @ManyToOne()
     @JoinColumn(name = "codCategoria")
     private ArticuloCategoria articuloCategoria;
 
