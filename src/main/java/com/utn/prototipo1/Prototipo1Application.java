7c65f8eeec4d1aa859e319bbc8f94c2515cc40b9
Nico <nicocarcano014@gmail.com>
2024-06-26 17:50:14 -0300

    hardcodeo de articulos

--- src/main/java/com/utn/prototipo1/Prototipo1Application.java
+++ src/main/java/com/utn/prototipo1/Prototipo1Application.java
@@ -2,7 +2,10 @@ package com.utn.prototipo1;
 
 import com.utn.prototipo1.moduloArticulo.entities.Articulo;
 import com.utn.prototipo1.moduloArticulo.entities.ArticuloCategoria;
+import com.utn.prototipo1.moduloArticulo.entities.TipoModeloInventario;
+import com.utn.prototipo1.moduloArticulo.repositories.ArticuloCategoriaRepository;
 import com.utn.prototipo1.moduloArticulo.repositories.ArticuloRepository;
+import com.utn.prototipo1.moduloArticulo.repositories.TipoModeloInventarioRepository;
 import com.utn.prototipo1.moduloVenta.entities.Factura;
 import com.utn.prototipo1.moduloVenta.repositories.FacturaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -24,48 +27,41 @@ public class Prototipo1Application {
 		System.out.println("Estoy funcionando");
 	}
 
-	/*@Bean
-	CommandLineRunner init(FacturaRepository facturaRepository) {
+	@Bean
+	CommandLineRunner init(TipoModeloInventarioRepository tipoModeloInventarioRepository, ArticuloCategoriaRepository articuloCategoriaRepository) {
 		return args -> {
 
-			Date fechaFactura1 = new GregorianCalendar(2024, Calendar.JANUARY, 19).getTime();
-			Date fechaFactura4 = new GregorianCalendar(2024, Calendar.FEBRUARY, 22).getTime();
-			Date fechaFactura3 = new GregorianCalendar(2023, Calendar.DECEMBER, 15).getTime();
-			Date fechaFactura5 = new GregorianCalendar(2023, Calendar.MARCH, 10).getTime();
-			Date fechaFactura7 = new GregorianCalendar(2023, Calendar.AUGUST, 20).getTime();
-			Date fechaFactura8 = new GregorianCalendar(2020, Calendar.DECEMBER, 16).getTime();
-			Date fechaFactura10 = new GregorianCalendar(2022, Calendar.FEBRUARY, 12).getTime();
-			Date fechaFactura100 = new GregorianCalendar(2022, Calendar.AUGUST, 28).getTime();
-			Date fechaFactura11 = new GregorianCalendar(2021, Calendar.FEBRUARY, 24).getTime();
-			Date fechaFactura6 = new GregorianCalendar(2018, Calendar.FEBRUARY, 3).getTime();
-			Date fechaFactura9 = new GregorianCalendar(2018, Calendar.AUGUST, 21).getTime();
+						TipoModeloInventario tipoModeloInventario1= TipoModeloInventario.builder()
+					.nombre("Lote fijo")
+					.build();
+
+			TipoModeloInventario tipoModeloInventario2= TipoModeloInventario.builder()
+					.nombre("Intervalo fijo")
+					.build();
+
+			tipoModeloInventarioRepository.save(tipoModeloInventario1);
+			tipoModeloInventarioRepository.save(tipoModeloInventario2);
 
 
-			Factura factura1 = Factura.builder().fechaFactura(fechaFactura9).build();
-			Factura factura2 = Factura.builder().fechaFactura(fechaFactura6).build();
-			Factura factura3 = Factura.builder().fechaFactura(fechaFactura8).build();
-			Factura factura4 = Factura.builder().fechaFactura(fechaFactura11).build();
-			Factura factura6 = Factura.builder().fechaFactura(fechaFactura10).build();
-			Factura factura7 = Factura.builder().fechaFactura(fechaFactura100).build();
-			Factura factura8 = Factura.builder().fechaFactura(fechaFactura7).build();
-			Factura factura9 = Factura.builder().fechaFactura(fechaFactura5).build();
-			Factura factura10 = Factura.builder().fechaFactura(fechaFactura3).build();
-			Factura factura11 = Factura.builder().fechaFactura(fechaFactura4).build();
-			Factura factura12 = Factura.builder().fechaFactura(fechaFactura1).build();
+		ArticuloCategoria articuloCategoria1 = ArticuloCategoria.builder()
+				.nombreCategoria("Electrodomesticos")
+				.tipoModeloInventario(tipoModeloInventario1)
+				.build();
 
-			facturaRepository.save(factura1);
-			facturaRepository.save(factura2);
-			facturaRepository.save(factura3);
-			facturaRepository.save(factura4);
-			facturaRepository.save(factura6);
-			facturaRepository.save(factura7);
-			facturaRepository.save(factura8);
-			facturaRepository.save(factura9);
-			facturaRepository.save(factura10);
-			facturaRepository.save(factura11);
-			facturaRepository.save(factura12);
+			ArticuloCategoria articuloCategoria2 = ArticuloCategoria.builder()
+					.nombreCategoria("Ropa")
+					.tipoModeloInventario(tipoModeloInventario2)
+					.build();
+
+			ArticuloCategoria articuloCategoria3 = ArticuloCategoria.builder()
+					.nombreCategoria("Herramientas")
+					.tipoModeloInventario(tipoModeloInventario2)
+					.build();
+			articuloCategoriaRepository.save(articuloCategoria1);
+			articuloCategoriaRepository.save(articuloCategoria2);
+			articuloCategoriaRepository.save(articuloCategoria3);
 	};
-}*/
+}
 
 }
 
