2560bba1978d23ef02275236627907483285d753
RodriSLK <sanzrodrigo36@gmail.com>
2024-07-01 13:39:16 -0300

    commit rodri ultimo

--- src/main/java/com/utn/prototipo1/Prototipo1Application.java
+++ src/main/java/com/utn/prototipo1/Prototipo1Application.java
@@ -34,6 +34,22 @@ public class Prototipo1Application {
 		SpringApplication.run(Prototipo1Application.class, args);
 		System.out.println("Estoy funcionando");
 	}
+    /*@Bean
+	CommandLineRunner init(ArticuloRepository articuloRepository, TipoModeloInventarioRepository tipoModeloInventarioRepository,
+						   ArticuloCategoriaRepository articuloCategoriaRepository, FacturaRepository facturaRepository) {
+        return args -> {
+
+            Date fechaFactura8 = new GregorianCalendar(2020, Calendar.DECEMBER, 16).getTime();
+            Date fechaFactura6 = new GregorianCalendar(2018, Calendar.FEBRUARY, 3).getTime();
+
+            Factura factura8 = Factura.builder().fechaFactura(fechaFactura8).build();
+            Factura factura6 = Factura.builder().fechaFactura(fechaFactura6).build();
+
+            facturaRepository.save(factura6);
+            facturaRepository.save(factura8);
+
+        };
+    }*/
 
 
 }
